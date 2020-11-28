09af02ffe8dd437c4cad393bcb871997fe283d40
bakeryproducts <gleb.m.sokolov@gmail.com>
2020-11-28 08:37:46 -0500

    port range fix

--- run.sh
+++ run.sh
@@ -27,7 +27,7 @@ docker run  \
             --name sokolov_ws \
             -di \
             -h sws \
-            -p 9000-10000:9000-10000 \
+            -p 9000-9100:9000-9100 \
             -v $PWD/workspace/$USERNAME:$WORKSPACE_HOME \
             -v $PWD/resources:$RESOURCES_PATH \
             --net sokolov_ws_net \
