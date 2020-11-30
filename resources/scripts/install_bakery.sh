2c6936297b4e52ab89fe95a86bb3d067172a823e
sokolov <gleb.m.sokolov@gmail.com>
2020-11-30 15:54:30 +0300

    Cleanup, added nbstripout

--- resources/scripts/install_bakery.sh
+++ resources/scripts/install_bakery.sh
@@ -20,34 +20,30 @@ cd $WORKSPACE_HOME/dists
 git clone https://github.com/bakeryproducts/dotfiles
 cd dotfiles 
 
-
 GIT_FOLDER=$WORKSPACE_HOME/dists
 
-cd ~
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cd ~/.oh-my-zsh
-git apply $GIT_FOLDER/dotfiles/.configs/zsh/agn.patch
+cd ~ && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd ~/.oh-my-zsh && git apply $GIT_FOLDER/dotfiles/.configs/zsh/agn.patch
 
 cd ~/.config
+ln -s $GIT_FOLDER/dotfiles/.configs/git git
 ln -s $GIT_FOLDER/dotfiles/.configs/zsh zsh
 ln -s $GIT_FOLDER/dotfiles/.configs/vim vim
 ln -s $GIT_FOLDER/dotfiles/.configs/xs xs
 
-cd $GIT_FOLDER/dotfiles/.configs/zsh/
-./completer
+pip3 install nbstripout
+nbstripout --install --global
+
+cd $GIT_FOLDER/dotfiles/.configs/zsh/ && ./completer
 
 vim +PlugInstall +qall  > /dev/null
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+cd $GIT_FOLDER/ && git clone https://github.com/clvv/fasd
+cd fasd && PREFIX=$HOME/.local make install
 
-cd $GIT_FOLDER/
-git clone https://github.com/clvv/fasd
-cd fasd
-PREFIX=$HOME/.local make install
 
 pip3 install -r /tmp/requirements.txt
-
 
 echo "bakery is installed"
